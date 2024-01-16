dc029f433148b6dfc85c74e2dc420409dd9f11f9
adpol95 <adpol95@gmail.com>
2024-01-16 19:13:31 +0300

    Finish upgrade mainText

--- modules/news/outterDatas.js
+++ modules/news/outterDatas.js
@@ -5,7 +5,6 @@ module.exports = async function () {
 
     const newsList = [];
     const {JSDOM} = jsdom;
-
     const mongoHasDatas = await News.find();
     if (!!mongoHasDatas[0] === false) {
 
@@ -15,23 +14,28 @@ module.exports = async function () {
             const dom = await new JSDOM(htmlText).window.document;
             console.log(i);
             const titles = dom.getElementsByClassName("post-card__title-link");
-            const images = dom.getElementsByClassName("post-card__featured-asset");
             const domens = dom.getElementsByClassName("post-card__image-link");
             const readyNews = [];
             for (let j = 0; j < titles.length; j++) {
                 const inTheNews = await (await fetch(domens[j].href)).text();
                 const domInTheNews = await new JSDOM(inTheNews).window.document;
                 const allImgs = [...domInTheNews.getElementsByClassName("article-main-section")[0].getElementsByTagName("img")].map(el => el.src);
+                const textContainer = domInTheNews.getElementsByClassName("post-single__content single__content entry-content")[0];
+                const mainTextProperNodes = [...textContainer.childNodes].filter(el =>
+                    el.nodeName === "P" ||
+                    el.nodeName === "H2" ||
+                    el.nodeName === "FIGURE" ||
+                    el.nodeName === "A" ||
+                    el.nodeName === "DIV");
                 readyNews.push({
-                    title: titles[j],
-                    listImg: images[j].childNodes[1].src,
+                    titleTop: titles[j].textContent,
                     dataTime: domInTheNews.getElementsByClassName("entry-date published")[0].textContent,
                     topImg: allImgs.shift(),
                     headerTitle: domInTheNews.getElementsByClassName("post-single__sub-header-text")[0].textContent,
                     authorName: domInTheNews.getElementsByClassName("author-name")[0].textContent,
                     authorDescription: domInTheNews.getElementsByClassName("author-description")[0].textContent,
                     mainImgs: allImgs,
-                    mainText: domInTheNews.getElementsByClassName("post-single__content single__content entry-content")[0].textContent,
+                    mainText: mainTextProperNodes.map(el => [el.nodeName, el.textContent]),
                 })
             }
             newsList.push(readyNews)
