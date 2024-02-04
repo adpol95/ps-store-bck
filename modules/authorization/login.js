4e8a61bb9fc9b5c3f8dc61dbc361142c62c511e6
adpol95 <adpol95@gmail.com>
2024-02-04 18:17:09 +0300

    Created login node, sending a ready token in response with success authentication 2.0

--- modules/authorization/login.js
+++ modules/authorization/login.js
@@ -9,9 +9,10 @@ async function login(req, res) {
     if (!mongoProfile) res.status(400).json({message: "Nickname or password does not match"});
     else if (mongoProfile["password"] !== password) res.status(401).json({message: "Password does not match"});
     else {
-        const accessToken = jwt.sign({mongoProfile}, process.env.ACCESS_TOKEN_SECRET);
+        const accessToken = jwt.sign({exp: 3600, data: { profileName: mongoProfile.userName }}, process.env.ACCESS_TOKEN_SECRET);
         res.status(200).json({message: "Welcome back!", token: accessToken});
     }
+
 
 }
 
