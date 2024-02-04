a49fc4a583e882a4fbdc849b91a8f4d41c024c79
adpol95 <adpol95@gmail.com>
2024-02-04 18:20:05 +0300

    Created login node, sending a ready token in response with success authentication 3.0

--- modules/authorization/login.js
+++ modules/authorization/login.js
@@ -5,7 +5,6 @@ async function login(req, res) {
 
     const {userName, password} = req.body;
     const mongoProfile = await User.findOne({'userName': userName});
-
     if (!mongoProfile) res.status(400).json({message: "Nickname or password does not match"});
     else if (mongoProfile["password"] !== password) res.status(401).json({message: "Password does not match"});
     else {
