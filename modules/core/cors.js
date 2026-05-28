7c2ecafc7fbfd4061c398dd4942adc692147dab2
adpol95 <adpol95@gmail.com>
2026-05-28 12:16:35 +0300

    feat: implement CORS middleware configuration for API origin security

--- modules/core/cors.js
+++ modules/core/cors.js
@@ -1,21 +1,22 @@
 module.exports = function cors(app) {
-    const cors = require('cors');
-    const options = {
-        origin: "*",
-        methods: "GET, PUT, POST, PATCH, DELETE",
-        allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+  const cors = require("cors");
+  const options = {
+    origin: "https://rebirth-store.netlify.app/",
+    methods: "GET, PUT, POST, PATCH, DELETE",
+    allowedHeaders:
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+  };
+  app.use(cors(options));
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+    );
+    if (req.method === "OPTION") {
+      res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+      return res.status(200).json({});
     }
-    app.use(cors(options));
-    // app.use((req, res, next) => {
-    //     res.header('Access-Control-Allow-Origin', '*');
-    //     res.header(
-    //         'Access-Control-Allow-Headers',
-    //         'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-    //     );
-    //     if (req.method === 'OPTION') {
-    //         res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-    //         return res.status(200).json({});
-    //     }
-    //     next();
-    // });
-}
+    next();
+  });
+};
