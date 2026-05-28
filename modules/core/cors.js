36db6e985cb236fb9e95d84d47a2674ccd415a8e
adpol95 <adpol95@gmail.com>
2026-05-28 12:22:57 +0300

    feat: implement CORS middleware for origin restriction and header configuration

--- modules/core/cors.js
+++ modules/core/cors.js
@@ -1,7 +1,7 @@
 module.exports = function cors(app) {
   const cors = require("cors");
   const options = {
-    origin: "https://rebirth-store.netlify.app/",
+    origin: "https://rebirth-store.netlify.app",
     methods: "GET, PUT, POST, PATCH, DELETE",
     allowedHeaders:
       "Origin, X-Requested-With, Content-Type, Accept, Authorization",
