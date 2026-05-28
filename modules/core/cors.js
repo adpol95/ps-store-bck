7420d48e8e63398fb543966839dc3a690ee3633b
adpol95 <adpol95@gmail.com>
2026-05-28 12:34:27 +0300

    feat: add cors configuration module to enable cross-origin requests for specified domains

--- modules/core/cors.js
+++ modules/core/cors.js
@@ -1,22 +1,15 @@
 module.exports = function cors(app) {
   const cors = require("cors");
   const options = {
-    origin: "https://rebirth-store.netlify.app",
-    methods: "GET, PUT, POST, PATCH, DELETE",
+    origin: [
+      "https://rebirth-store.netlify.app",
+      "http://localhost:5173",
+      "http://localhost:3000"
+    ],
+    credentials: true,
+    methods: "GET, PUT, POST, PATCH, DELETE, OPTIONS",
     allowedHeaders:
       "Origin, X-Requested-With, Content-Type, Accept, Authorization",
   };
   app.use(cors(options));
-  app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization",
-    );
-    if (req.method === "OPTION") {
-      res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-      return res.status(200).json({});
-    }
-    next();
-  });
 };
